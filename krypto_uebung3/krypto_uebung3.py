114ac1600734a324fd90c874443f02deaf57541f
Daniel Wittmann <fini4631@uni-weimar.de>
2015-05-05 20:48:52 +0200

    erste funktionen implementiert
    Die Datei wird als Kommandozeilen Parameter übergeben. Funktioniert
    jedoch noch nicht ganz (ist aber denke Windows-Problem denn Pfad
    entspricht nicht den unicode). Funktion create_sbox erstellt nur eine
    s_box mit null-Elementen.

---
+++ krypto_uebung3/krypto_uebung3.py
@@ -0,0 +1,52 @@
+# Martin Heinrich 
+# Daniel Wittmann 113017
+#
+# Python-Version 3.4.3
+
+#***************************************************************************
+# Python-Script zur Lösung der Aufgabe 5 auf den 3. Übungsblatt. Es wird 
+# eine Differential Distribution Table, einer gegebenen S-Box erstellt. Die 
+#S-box wird als .txt - Datei als Kommandozeilen Parameter entgegen genommen.
+#***************************************************************************
+
+import sys
+#sbox_data = open(sys.argv[1], 'r').read()
+sbox_data = open('s_box.txt', 'r').read() 
+
+#***************************************************************************
+# Initialisiert eine Matrix, in welcher die Werte der DDT eingetragen werde.
+def create_matrix (string) :
+    string.split('\n')
+    x = 2** int(string[2])
+    y = 2** int(string[0])
+    matrix = [[0 for i in range(x)] for j in range(y)]
+
+    return matrix
+#***************************************************************************
+
+#***************************************************************************
+# Erstellt die Sbox in form einer zwei-dimensionalen Liste. Erste Spalte, 
+# enthält den Sbox-Input, die zweite Spalte den Sbox-Output.
+def create_sbox (string) :
+    string.split('\n')
+    x = 2** int(string[0])
+
+    sbox = [[0 for i in range(x)]for j in range(2)]
+
+    for i in range(x):
+        sbox [i][0] = i
+
+    temp_list = string[4]
+    temp_list.split(',')
+
+    for i in range(0,len(temp_list),2):
+        sbox[0][i] = int(temp_list[i],16)
+
+    return sbox
+#***************************************************************************
+
+#def print_matrix(matrix) :
+    
+sbox = create_sbox(sbox_data)
+print(sbox)
+
